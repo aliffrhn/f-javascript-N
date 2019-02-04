9420e225059ae147a9ffd1bea13e7b8d9cb44070
aliffrhn <alif.farhanw@gmail.com>
2019-02-05 02:15:54 +0700

    add branch and tes var let

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ var message = "Hello";
 let msg1 = "mestes 1";
 const msg2 = "mestes 2";
 
+if(true){
+    var message = 1;
+    let msg1 = 2;
+    console.log("tes", msg1);
+}
+
+console.log(msg1);
 console.log(message);
