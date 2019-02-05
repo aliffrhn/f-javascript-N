d0f08bfc77f7fa4d9e9dfd33ac0819f4367e3481
aliffrhn <alif.farhanw@gmail.com>
2019-02-05 18:07:12 +0700

    add sc test

---
+++ switchcase.js
@@ -0,0 +1,16 @@
+function testSwitchCase(){
+    var n = 3;
+
+    switch(n){
+        case 3:
+            console.log("3")
+        break;
+        case 10:
+            console.log("10")
+        break;
+        default:
+            console.log("ELSE")
+    }
+}
+
+testSwitchCase()
