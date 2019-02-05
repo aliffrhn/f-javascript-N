82e761374aec8ad4be61aaba3cda73588a4703df
aliffrhn <alif.farhanw@gmail.com>
2019-02-05 17:51:52 +0700

    try new f ifelse

--- ifelse.js
+++ ifelse.js
@@ -1,11 +1,14 @@
 function jalanSatu(){
     var nilai = 3;
 
-    if(nilai === 3){
-        console.log("T")
-    }else{
-        console.log("F")
-    }
+    // if(nilai === 3){
+    //     console.log("T")
+    // }else{
+    //     console.log("F")
+    // }
+    
+    if(nilai === 3)console.log("Test")
+    else console.log("B")
 }
 
 jalanSatu()
