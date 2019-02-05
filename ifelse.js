7ba156bdce2a80647cf57ca14a985ef46a50db15
aliffrhn <alif.farhanw@gmail.com>
2019-02-05 17:49:25 +0700

    l change

--- ifelse.js
+++ ifelse.js
@@ -2,9 +2,9 @@ function jalanSatu(){
     var nilai = 3;
 
     if(nilai === 3){
-        console.log("T");
+        console.log("T")
     }else{
-        console.log("F");
+        console.log("F")
     }
 }
 
